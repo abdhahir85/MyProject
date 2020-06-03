1a17f6e7e62eb3c3d4d432ddbcdeb1cdf13344f7
Alhajeera <Alhajeera@Alhajeera-PC>
2020-06-03 13:02:10 +0530

    add

--- src/programs/Armsrong.java
+++ src/programs/Armsrong.java
@@ -5,12 +5,18 @@ public class Armsrong {
 	public static void main(String[] args) {
 		
 		int n=153;
+		int temp=n;
 		int arm=0;
+		for(int i=0;i<1000;i++){
+			n=i;
 		while(n>0){
 			arm=arm+((n%10)*(n%10)*(n%10));
 			n=n/10;
-			System.out.println(arm);
-
+		}
+		if(arm==i){
+			System.out.println("The given number is armstrong no" + i);
+		}
+		arm=0;
 		}
 	}
 
